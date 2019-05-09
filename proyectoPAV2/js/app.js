ff4c74df2a1888f7138867a5dd32049a2aedcf21
FranciscoBarafani <42209172+FranciscoBarafani@users.noreply.github.com>
2019-05-08 22:07:25 -0300

    Small Update
    ADded Show Hide function

--- proyectoPAV2/js/app.js
+++ proyectoPAV2/js/app.js
@@ -1,5 +1,27 @@
 ﻿//Angular-JS
 myApp = angular.module('myApp', []);
 myApp.controller('index', function ($scope) {
-    
+
+    //Show And Hide Functions  //CREAR UNA FUNCION UNICA POR ID??
+    //Variables Initiation
+    $scope.shExpedients = true; //Starts with true as default
+    $scope.shNew = false;
+    $scope.shModify = false;
+    //sh references ShowHide (variable with flag to show or hide div)
+    $scope.ShowHideExpedients = function () {
+        $scope.shExpedients = true;
+        $scope.shNew = false;
+        $scope.shModify = false;
+    }
+    $scope.ShowHideNew = function () {
+        $scope.shExpedients = false;
+        $scope.shNew = true;
+        $scope.shModify = false;
+    }
+    $scope.ShowHideModify = function () {
+        $scope.shExpedients = false;
+        $scope.shNew = false;
+        $scope.shModify = true;
+    }
+
 });
